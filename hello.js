6d82ebc24e9eac93780276bc06d0d6d852e60f3d
abdullahubaids <94051662+abdullahubaids@users.noreply.github.com>
2022-09-27 14:37:12 +0700

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,29 @@
+var pesan = "hallo dunia!!!!";
+console.log(pesan);
+var manusia1 = {
+	nama : "Abdullah",
+	umur : 21,
+	hobi : ["main","lomba","gigitaran"],
+};
+var manusia2 = {
+	nama : "agam",
+	umur : 21,
+	hobi : ["makan","ngopi"],
+};
+var dulur = [manusia1,manusia2]
+console.log(dulur);
+
+var negara1 = {
+	nama : "indonesia",
+	ibukota : "jakarta",
+	benua : "asia",
+	pulau : ["sumatra","jawa","kalimantan","sulawesi","papua"],
+};
+var negara2 = {
+	nama : "indonesia",
+	ibukota : "jakarta",
+	benua : "asia",
+	pulau : ["sumatra","jawa","kalimantan","sulawesi","papua"],
+};
+var negara = [negara1,negara2];
+console.log(negara);
